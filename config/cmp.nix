70046197f8bf8baa4a32cc243fa537a2bb76a850
Nicholas Brown <countdooku.steak@gmail.com>
2024-07-07 18:13:38 -0700

    remove unneeded cmp plugins

--- config/cmp.nix
+++ config/cmp.nix
@@ -5,15 +5,11 @@
 
     cmp-buffer = { enable = true; };
 
-    cmp-emoji = { enable = true; };
-
     cmp-nvim-lsp = { enable = true; };
 
     cmp-path = { enable = true; };
 
     cmp_luasnip = { enable = true; };
-
-    cmp-cmdline = { enable = true; };
 
     cmp = {
       enable = true;
