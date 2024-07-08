fae16096b459937bceed5f0f3a5a49f854c3df6e
Nicholas Brown <countdooku.steak@gmail.com>
2024-07-08 00:20:11 -0700

    open Neotree by default for directories

--- config/utils/neo-tree.nix
+++ config/utils/neo-tree.nix
@@ -5,9 +5,28 @@
 
   };
   autoCmd = [
-    # Update git status when closing lazygit
     {
       event = [ "TermClose" ];
+      desc = "Open neo-tree on enter for directories";
+      once = true;
+
+      callback = {
+        __raw =
+          # lua
+          ''
+            function()
+                local stats = vim.uv.fs_stat(vim.fn.argv(0))
+                if stats and stats.type == "directory" then
+                  vim.cmd "Neotree"
+                  vim.cmd "wincmd l"
+                end
+            end
+          '';
+      };
+    }
+    {
+      event = [ "TermClose" ];
+      desc = "Update neo-tree gitstatus when closing lazygit";
 
       callback = {
         __raw =
