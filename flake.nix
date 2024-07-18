3332bd760e8c629371b0a449735296ed543fc688
Pouya Rezaeifakhr <pouya@sonic.net>
2024-07-18 11:59:43 -0700

    updated neovim to point to more recent nixpkgs-unstable. This PR resolves issues with plugin dependencies: https://github.com/NixOS/nixpkgs/pull/319325
    This PR resolves the jsregexp missing dependency for luasnip. The issue
    was also resolved when using:
    
    ```bash
      nix-shell -p luajitPackages.jsregexp
      nvim
    ````
    
    When I included the luajitPackages.jsregexp inside nixos-config or the
    extra-packages in nixvim-config the warning still remained.

--- flake.nix
+++ flake.nix
@@ -3,22 +3,17 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
     nixvim.url = "github:nix-community/nixvim";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs =
-    { nixvim, flake-parts, ... }@inputs:
+  outputs = { nixvim, flake-parts, ... }@inputs:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
+      systems =
+        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
 
-      perSystem =
-        { pkgs, system, ... }:
+      perSystem = { pkgs, system, ... }:
         let
           nixvimLib = nixvim.lib.${system};
           nixvim' = nixvim.legacyPackages.${system};
@@ -28,14 +23,15 @@
             # You can use `extraSpecialArgs` to pass additional arguments to your module files
             extraSpecialArgs = {
               # inherit (inputs) foo;
+              nixpkgs-stable = inputs.nixpkgs-stable.legacyPackages.${system};
             };
           };
           nvim = nixvim'.makeNixvimWithModule nixvimModule;
-        in
-        {
+        in {
           checks = {
             # Run `nix flake check .` to verify that your config is not broken
-            default = nixvimLib.check.mkTestDerivationFromNixvimModule nixvimModule;
+            default =
+              nixvimLib.check.mkTestDerivationFromNixvimModule nixvimModule;
           };
 
           packages = {
