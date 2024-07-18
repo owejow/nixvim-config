173159f62da7f5041e7c1cafee47a69898a7c517
Pouya Rezaeifakhr <pouya@sonic.net>
2024-07-17 22:00:41 -0700

    changed inc_rename option to false for noice

--- config/utils/noice.nix
+++ config/utils/noice.nix
@@ -3,13 +3,15 @@
     enable = true;
     notify = { enabled = false; };
     messages = { enabled = true; };
+
     presets = {
       bottom_search = true;
       command_palette = false;
       long_message_to_split = true;
-      inc_rename = true;
+      inc_rename = false;
       lsp_doc_border = false;
     };
+
     lsp = {
       message = { enabled = true; };
       progress = {
