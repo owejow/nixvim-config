0a5d8c0458ffd500c803ea3c0f32c57bc066f5c9
Pouya Rezaeifakhr <pouya@sonic.net>
2024-07-23 11:48:32 -0700

    removed unnecessary dependency

--- config/extra-files/default.nix
+++ config/extra-files/default.nix
@@ -1,4 +1,4 @@
-{ lib, ... }: {
+{
   extraFiles = {
     # "lua/colorscheme-helpers.lua".text = lib.readFile ./colorscheme-helpers.lua;
     "lua/colorscheme-helpers.lua".source = ./colorscheme-helpers.lua;
