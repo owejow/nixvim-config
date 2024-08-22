c507de77065d5985daadc8805dcbdc7626b06c54
Pouya Rezaeifakhr <pouya@sonic.net>
2024-08-21 17:12:51 -0700

    added shortcut for markview toggling

--- config/languages/markview/extra-lua-config.lua
+++ config/languages/markview/extra-lua-config.lua
@@ -1,4 +1,6 @@
 local markview = require("markview")
+local wk = require("which-key")
+
 markview.setup({
   -- This is nice to have
   mode = { "n", "x" },
@@ -8,4 +10,20 @@ markview.setup({
       vim.wo[win].concealcursor = "nc"
     end,
   },
+})
+
+local is_markdown_filetype = function()
+  local buf = vim.api.nvim_get_current_buf()
+  local ft = vim.api.nvim_buf_get_option(buf, "filetype")
+  return ft == "markdown"
+end
+
+wk.add({
+  "<leader>md",
+  function()
+    if is_markdown_filetype() then
+      vim.api.nvim_command("Markview toggle")
+    end
+  end,
+  desc = "Toggle Markview",
 })
