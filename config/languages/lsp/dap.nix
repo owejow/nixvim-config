de9eff72b5c2c34e1ecedf9d76a4ad9c281e75a6
Pouya Rezaeifakhr <pouya@sonic.net>
2025-01-27 15:16:14 -0800

    updated plugins

--- config/languages/lsp/dap.nix
+++ config/languages/lsp/dap.nix
@@ -1,11 +1,6 @@
 inputs: {
   plugins = {
-    dap = {
-      enable = true;
-      extensions = {
-        dap-ui = { enable = true; };
-        dap-virtual-text = { enable = true; };
-      };
-    };
+    dap = { enable = true; };
+    dap-ui = { enable = true; };
   };
 }
