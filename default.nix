8cf9db6957761abf4c2fbaf55d11393bc4475856
Pouya Rezaeifakhr <pouya@sonic.net>
2025-06-04 17:05:05 -0700

    added linting

---
+++ default.nix
@@ -0,0 +1,18 @@
+{ lib, ... }:
+{
+  plugins.nvim-tree = {
+    enable = true;
+  };
+
+  extraConfigLua = lib.readFile ./extra-lua-config.lua;
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>e";
+      action = "<cmd>NvimTreeToggle<cr>";
+      options = {
+        desc = "Toggle Neotree";
+      };
+    }
+  ];
+}
