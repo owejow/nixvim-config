dcd63d1ce9a44da0b5be22261b33bf4b34fdd098
Pouya Rezaeifakhr <pouya@sonic.net>
2025-06-06 12:28:12 -0700

    added special settings for layzdev, still did not fix the uv.fs_stat missiing keyword warning

--- config/plugins/lazydev/default.nix
+++ config/plugins/lazydev/default.nix
@@ -1,7 +1,16 @@
 {
   plugins = {
     lazydev = {
-      enable = true;
+      enable = false;
+      settings = {
+        enabled = true;
+        library = [
+          {
+            path = "\${3rd}luv/library";
+            words = [ "vim%.uv" ];
+          }
+        ];
+      };
     };
   };
 }
